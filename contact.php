d35eb19ecd59776c919d0708ac5e3217a13bc2fa
Kundan <kundankumar594@gmail.com>
2025-09-25 21:40:55 -0400

    contact structure has been added

--- contact.php
+++ contact.php
@@ -10,7 +10,73 @@
 	<body>
 		<?php include "includes/header.php"; ?>
 		<main class="main">
-			<h1>Heading: Primary</h1>
+			<div class="background-block background-block--contact">
+				<section class="container-mid">
+					<div class="background-block-content address-block">
+						<div class="heading-block">
+							<h1 class="heading-primary">Get in touch</h1>
+							<div class="sub-heading">We're in an old heritage building, fringed with palm trees, surrounded by beautiful views.</div>							
+						</div>
+						<h2 class="heading-secondary">Our Location</h2>					
+						<p>7834 Lake Avenue, Merrick, NY 11566</p>		
+						<p><a href="mailto:hello@liquidapt.com">hello@liquidapt.com</a></p>			
+					</div> <!-- /address-block -->
+				</section>
+				<div class="overlay"></div>					
+			</div> <!-- /background-block -->
+
+			<div class="email-block">
+				<div class="container-main">
+					<div class="grid-parent">
+						<div class="grid-child email-each">
+							<div class="email-each-type">Jobs</div>
+							<a href="mailto:job@liquidapt.com">
+								<img src="http://placehold.it/32x32" alt="">
+								<div><strong>jobs@liquidapt.com</strong></div>
+							</a>
+						</div> <!-- /email-each -->
+
+						<div class="grid-child email-each">
+							<div class="email-each-type">Support</div>
+							<a href="mailto:support@liquidapt.com">
+								<img src="http://placehold.it/32x32" alt="">
+								<div><strong>support@liquidapt.com</strong></div>
+							</a>
+						</div> <!-- /email-each -->
+
+						<div class="grid-child email-each">
+							<div class="email-each-type">Theme creation</div>
+							<a href="mailto:create@liquidapt.com">
+								<img src="http://placehold.it/32x32" alt="">
+								<div><strong>create@liquidapt.com</strong></div>
+							</a>
+						</div> <!-- /email-each -->
+				  </div> <!-- /grid-parent -->
+			  </div><!-- /container-main -->
+		  </div> <!-- /email-block -->
+
+			<div class="map-google-embed">
+			</div>
+
+			<div class="social-block">			
+				<div class="container-main">
+					<a href="" class="social-icon-each">
+			  		<img src="http://placehold.it/72x72" alt="social-icon-twitter">
+			  	</a>
+			  	
+			  	<a href="" class="social-icon-each">
+			  		<img src="http://placehold.it/72x72" alt="social-icon-facebook">
+			  	</a>
+
+			  	<a href="" class="social-icon-each">
+			  		<img src="http://placehold.it/72x72" alt="social-icon-googleplus">
+			  	</a>
+
+			  	<a href="" class="social-icon-each">
+			  		<img src="http://placehold.it/72x72" alt="social-icon-skype">
+			  	</a>				
+		  	</div> <!-- /container-main -->
+			</div> <!-- /social-block -->
 		</main>
 		<?php include "includes/footer.php"; ?>
 		<?php include "includes/js-global.php"; ?>

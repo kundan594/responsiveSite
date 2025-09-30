6830f3a669737c57c10d003958f65dad8edf5451
Kundan <kundankumar594@gmail.com>
2025-09-29 21:07:03 -0400

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
+// NAVIGATION: OFF CANVAS
+
+// $(".nav-trigger").on("click", function(e){
+// 	e.preventDefault();
+// 	e.stopPropagation();
+// 	$(".nav-block").toggleClass("nav-block-open");
+// });
+
+// $(".nav-block").on("click", function(e){
+// 	e.stopPropagation();
+// });
+
+// $(window).on("click", function(){
+// 	$(".nav-block").removeClass("nav-block-open");
+// });
+
+
 // ACTIVE NAV STYLING
 
 $(".nav li a").each(function(){

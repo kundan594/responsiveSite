c30c93ab74b89dd5eebb0fec3499cb514b26e81e
Kundan <kundankumar594@gmail.com>
2025-09-29 20:52:53 -0400

    header update

--- index.php
+++ index.php
@@ -10,9 +10,25 @@
 	<body>
 		<?php include "includes/header.php"; ?>
 		<main class="main">
-			<h1>Heading: Primary</h1>
+			<section class="container-main">
+				<div class="center">
+					<div class="banner-home">
+						<div class="heading-block">
+							<h1 class="heading-primary heading-primary--banner">Drag . Drop . Build.</h1>
+							<div class="sub-heading sub-heading--banner">Create a free, responsive site in minutes!</div>
+						</div>
+						<div class="banner-img-block">
+			  			<img src="http://placehold.it/280x442" alt="" class="banner-img banner-img-1" width="280">
+			  			<img src="http://placehold.it/280x442" alt="" class="banner-img banner-img-2" width="280">
+			  			<img src="http://placehold.it/280x442" alt="" class="banner-img banner-img-3" width="280">
+						</div> <!-- /banner-img-block -->
+						<a href="overview.php" class="button button-explore">Explore</a>
+						<a href="" class="button">Get LiquidApt Now</a>				
+					</div> <!-- /banner-home -->
+				</div> <!-- /center -->
+			</section> <!-- /container-main -->
 		</main>
-		<?php include "includes/footer.php"; ?>
+
 		<?php include "includes/js-global.php"; ?>
 	</body>
 	

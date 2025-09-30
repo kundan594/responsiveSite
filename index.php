d828f5a51c0f952a4469322306d2f06aaccc58d6
Kundan <kundankumar594@gmail.com>
2025-09-29 20:55:58 -0400

    clearfix added

--- index.php
+++ index.php
@@ -17,12 +17,12 @@
 							<h1 class="heading-primary heading-primary--banner">Drag . Drop . Build.</h1>
 							<div class="sub-heading sub-heading--banner">Create a free, responsive site in minutes!</div>
 						</div>
-						<div class="banner-img-block">
+						<div class="banner-img-block clearfix">
 			  			<img src="http://placehold.it/280x442" alt="" class="banner-img banner-img-1" width="280">
 			  			<img src="http://placehold.it/280x442" alt="" class="banner-img banner-img-2" width="280">
 			  			<img src="http://placehold.it/280x442" alt="" class="banner-img banner-img-3" width="280">
 						</div> <!-- /banner-img-block -->
-						<a href="overview.php" class="button button-explore">Explore</a>
+						<a href="overview.php" class="button button-hollow button-explore">Explore</a>
 						<a href="" class="button">Get LiquidApt Now</a>				
 					</div> <!-- /banner-home -->
 				</div> <!-- /center -->

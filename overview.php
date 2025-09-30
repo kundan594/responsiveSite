2aa8cff282b6096689bfc65d0b46f7ddf09c4ec5
Kundan <kundankumar594@gmail.com>
2025-09-29 20:28:56 -0400

    overview page has been added and it blocks

--- overview.php
+++ overview.php
@@ -10,7 +10,170 @@
 	<body>
 		<?php include "includes/header.php"; ?>
 		<main class="main">
-			<h1>Heading: Primary</h1>
+			<div class="overview-video-block">
+				<section class="container-mid">
+					<div class="heading-block">
+						<h1 class="heading-primary">Overview</h1>				
+						<h2 class="heading-secondary">Watch this video &amp; get started with LiquidApt!</h2>
+						<div class="sub-heading">Made by Dimitri Smirnov, one of our early adopters!</div>
+					</div>
+
+					<div class="iframe-video-block">
+						<iframe src="https://player.vimeo.com/video/57260119" width="900" height="506" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>				
+					</div>
+				</section>				
+			</div> <!-- /overview-video-block -->
+
+			<section class="liquidapt-steps-block">
+				<div class="container-main">
+					<div class="heading-block">
+						<h2 class="heading-secondary">Get your site up and running!</h2>
+						<div class="sub-heading">In 3 easy steps</div>						
+					</div>
+					<div class="grid-parent">
+						<a href="" class="liquidapt-steps-each grid-child">
+							<img src="http://placehold.it/200x200" alt="" class="liquidapt-steps-graphic">
+							<div class="liquidapt-steps-info">Sign up</div>
+							<div class="font-smaller">Step 1</div>
+						</a>
+
+						<a href="" class="liquidapt-steps-each grid-child">
+							<img src="http://placehold.it/200x200" alt="" class="liquidapt-steps-graphic">
+							<div class="liquidapt-steps-info">Select a theme</div>
+							<div class="font-smaller">Step 2</div>
+						</a>
+
+						<a href="" class="liquidapt-steps-each grid-child">
+							<img src="http://placehold.it/200x200" alt="" class="liquidapt-steps-graphic">
+							<div class="liquidapt-steps-info">Customize your site</div>
+							<div class="font-smaller">Step 3</div>
+						</a>						
+					</div> <!-- /grid-parent -->
+				</div> <!-- /container-main -->
+			</section> <!-- /liquidapt-steps-block -->							
+
+			<section>
+				<h2 class="heading-secondary">Why use LiquidApt?</h2>	
+				<div class="liquidapt-usage-block">
+					<div class="liquidapt-usage-each clearfix">
+						<div class="liquidapt-usage-info">
+							<ul>
+								<li>12 gorgeous, responsive themes</li>
+								<li>1-4 new themes released every month</li>
+								<li>Starter theme for coders who want complete control</li>
+							</ul>
+						</div>
+
+						<div class="liquidapt-usage-img">
+							<img src="http://placehold.it/1000x667" alt="">
+						</div>
+					</div> <!-- /liquidapt-usage-each -->
+
+					<div class="liquidapt-usage-each clearfix">
+						<div class="liquidapt-usage-info">
+							<ul>
+								<li>3 themes for an admin interface or dashboard</li>
+								<li>Add bar, pie, bubble charts and more!</li>
+								<li>Impress clients with superb visuals for stats</li>
+							</ul>
+						</div>
+
+						<div class="liquidapt-usage-img">
+							<img src="http://placehold.it/1000x667" alt="">
+						</div>
+					</div> <!-- /liquidapt-usage-each -->
+
+					<div class="liquidapt-usage-each clearfix">
+						<div class="liquidapt-usage-info">
+							<ul>
+								<li>Edit your site on the go!</li>
+								<li>Log into our CMS from any device!</li>
+								<li>Or download our LiquidApt app on iOS, Android or Windows.</li>
+							</ul>
+						</div>
+
+						<div class="liquidapt-usage-img">
+							<img src="http://placehold.it/1000x667" alt="">
+						</div>
+					</div> <!-- /liquidapt-usage-each -->
+				</div> <!-- /liquidapt-usage-block -->					
+			</section>					
+
+			<section class="liquidapt-theme-block">
+				<div class="container-mid">
+					<div class="heading-block">
+						<h2 class="heading-secondary">Production-ready templates</h2>
+						<div class="sub-heading">Have your website up in an hour!</div>
+					</div>
+				  <table class="table table-liquidapt-theme table-responsive">
+		 		  	<thead>
+				  		<tr>
+				  			<th>Themes</th>
+				  			<th>Features</th>
+				  			<th>Pricing</th>
+				  			<th>Templates</th>
+				  		</tr>
+				  	</thead>
+				  	<tbody>
+				  		<tr>
+					  		<td>Portfolio</td>
+				  			<td>
+					  			<ul class="ul-base">
+										<li>Unlimited image galleries</li>
+										<li>Responsive videos and embeds</li>
+										<li>Custom filters for sections</li>
+										<li>Beautiful, scrolling animations</li>
+									</ul>
+				  			</td>
+				  			<td>Free!</td>
+				  			<td>12</td>
+				  		</tr>
+				  		<tr>
+					  		<td>Events</td>
+					  		<td>
+					  			<ul class="ul-base">
+					  				<li>Responsive calendar widget</li>
+					  				<li>Event booking</li>
+					  				<li>Pre-registration</li>
+					  				<li>Ticketing options</li>
+					  			</ul>
+					  		</td>
+				  			<td>Free!</td>
+				  			<td>8</td>
+				  		</tr>
+				  		<tr>
+					  		<td>E-commerce</td>
+					  		<td>
+					  			<ul class="ul-base">
+						  			<li>Product zoom-in feature</li>
+						  			<li>One-page checkout</li>
+						  			<li>List or grid format for products</li>
+						  			<li>Create discounts on products</li>
+						  		</ul>
+					  		</td>
+				  			<td>Free!</td>
+				  			<td>4</td>
+				  		</tr>
+				  	</tbody>
+				  </table>
+				</div> <!-- /container-mid -->
+			</section> <!-- /liquidapt-theme-block -->
+
+			<section class="overview-sign-up-block">
+				<div class="container-full">
+				<div class="heading-block">
+					<h2 class="heading-secondary">We're free, no strings attached.</h2>
+					<div class="sub-heading">Always. And forever.</div>					
+				</div>
+
+				<form>
+					<div class="field-block center">
+						<a href="" class="button">Get Started!</a>					
+					</div>
+				</form>
+				</div>
+			</section> <!-- /overview-sign-up-block -->
+
 		</main>
 		<?php include "includes/footer.php"; ?>
 		<?php include "includes/js-global.php"; ?>
